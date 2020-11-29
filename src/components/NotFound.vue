0b198337549879b17057fc8a11d369cd458eb515
jlambdin <jlambdin@rams.colostate.edu>
2020-11-28 20:32:10 -0700

    13.2 11/19 Axios, signup route

---
+++ src/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Not Found</h1>
+        <h4>Sorry, this route cannot be found</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import router from './routes'
 
+
+axios.defaults.baseURL = "https://cis410-fa20-rockwellapi.azurewebsites.net"
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
